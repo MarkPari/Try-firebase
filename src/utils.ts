0fc0ad2d3555f66a0231cfd6ca4091b0f7a699dc
MarkPari <marco.parisi@stevejobs.academy>
2022-03-24 18:22:41 +0100

    feat(document): remove secret key

--- src/utils.ts
+++ src/utils.ts
@@ -9,39 +9,5 @@ export const formatCollection = <T>(collection: FirebaseFirestore.QuerySnapshot<
 }
 
 
-// class Post {
-//     constructor(readonly title: string, readonly author: string) {}
-  
-//     toString(): string {
-//       return this.title + ', by ' + this.author;
-//     }
-//   }
-  
-//   const postConverter = {
-//     toFirestore(post: Post): firebase.firestore.DocumentData {
-//       return {title: post.title, author: post.author};
-//     },
-//     fromFirestore(
-//       snapshot: firebase.firestore.QueryDocumentSnapshot,
-//       options: firebase.firestore.SnapshotOptions
-//     ): Post {
-//       const data = snapshot.data(options)!;
-//       return new Post(data.title, data.author);
-//     }
-//   };
-  
-//   const postSnap = await firebase.firestore()
-//     .collection('posts')
-//     .withConverter(postConverter)
-//     .doc().get();
-//   const post = postSnap.data();
-//   if (post !== undefined) {
-//     post.title; // string
-//     post.toString(); // Should be defined
-//     post.someNonExistentProperty; // TS error
-//   }
-// export const concatCollection = (dat: FirebaseFirestore.Firestore,) => {
-//     const db = 
-// }
 
 
